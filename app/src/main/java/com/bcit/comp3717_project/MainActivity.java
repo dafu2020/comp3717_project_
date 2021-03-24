9a7542eecac7c068cd7d83e1a4c65beccb1cf2ae
dafu2020 <xlu49@my.bcit.ca>
2021-03-24 00:51:05 -0700

    added Login method

--- app/src/main/java/com/bcit/comp3717_project/MainActivity.java
+++ app/src/main/java/com/bcit/comp3717_project/MainActivity.java
@@ -1,23 +1,51 @@
 package com.bcit.comp3717_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity{
 
     private TextView register;
+    private EditText editTextEmail, editTextPassword;
+    private Button loginBtn;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = (EditText) findViewById(R.id.main_editText_email);
+        editTextPassword = (EditText) findViewById(R.id.main_editText_Password);
+
+
         register = (TextView) findViewById(R.id.main_tv_register);
         register.setOnClickListener(registerOnClick);
+
+        loginBtn = (Button) findViewById(R.id.main_btn_login);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userLogin();
+            }
+        });
     }
 
     private final View.OnClickListener registerOnClick = new View.OnClickListener()
@@ -30,4 +58,51 @@ public class MainActivity extends AppCompatActivity{
             startActivity(registerIntent);
         }
     };
+
+    private void userLogin(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+
+        if(email.isEmpty()) {
+            editTextEmail.setError("Email is required for registration!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please provide valid email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()) {
+            editTextPassword.setError("Password is required for registration!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6) {
+            editTextPassword.setError("Password need to be more than 6 characters!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent homeIntent  = new Intent(MainActivity.this,
+                            HomePageActivity.class);
+                    startActivity(homeIntent);
+
+                }else {
+                    Toast.makeText(MainActivity.this,
+                            "Failed to login! Please CHECK YOUR CREDENTIAL ",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
 }
