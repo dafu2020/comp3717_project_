f50d7b6347db410d2a693d25a3eae0c4796855d0
dafu2020 <xlu49@my.bcit.ca>
2021-03-24 13:22:29 -0700

    minor updates

--- app/src/main/java/com/bcit/comp3717_project/User.java
+++ app/src/main/java/com/bcit/comp3717_project/User.java
@@ -1,20 +1,46 @@
 package com.bcit.comp3717_project;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class User {
    public String name, email;
 
-    ArrayList<User> friendList = new ArrayList<User>();
+    private List<User> friendList;
+    private List<User> eventList;
+
 
     public User(){
-
     }
 
     public User(String name, String email){
         this.name = name;
         this.email = email;
+        this.friendList = new ArrayList<User>();
     }
 
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<User> getFriendList() {
+        return friendList;
+    }
+
+    public void setFriendList(List<User> friendList) {
+        this.friendList = friendList;
+    }
 }
